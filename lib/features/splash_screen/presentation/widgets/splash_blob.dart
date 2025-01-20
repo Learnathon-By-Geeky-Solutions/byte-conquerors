b527a5cc6196ef090054033ce899e1b86bff4e36
Dev Ruman <sayfurrahman324@gmail.com>
2025-01-21 00:34:56 +0600

    binding problem fixed

--- lib/features/splash_screen/presentation/widgets/splash_blob.dart
+++ lib/features/splash_screen/presentation/widgets/splash_blob.dart
@@ -1,18 +1,23 @@
-import 'package:blobs/blobs.dart';
 import 'package:flutter/material.dart';
-import 'package:soul_space/core/config/theme/app_colors.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:soul_space/core/config/assets/app_vectors.dart';
 
 class SplashBlob extends StatelessWidget {
   const SplashBlob({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Blob.fromID(
-      id: ['3-7-742289'],
-      size: 400,
-      styles: BlobStyles(
-        color: AppColors.primaryGreen,
-      ),
-    );
+  Widget build(
+    BuildContext context,
+  ) {
+    // final Size windowSize = MediaQuery.of(context).size;
+    return SvgPicture.asset(
+      AppVectors.splashBlob,
+    ).animate().scale(
+          delay: 500.ms,
+          duration: 1000.ms,
+          end: Offset(10, 10),
+          curve: Curves.easeOut,
+        );
   }
 }
--- lib/features/splash_screen/presentation/widgets/splash_logo.dart
+++ lib/features/splash_screen/presentation/widgets/splash_logo.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:soul_space/core/config/theme/assets/app_vectors.dart';
+import 'package:soul_space/core/config/assets/app_vectors.dart';
 
 class SplashLogo extends StatelessWidget {
   const SplashLogo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SvgPicture.asset(AppVectors.spotifyLogo);
+    return SvgPicture.asset(AppVectors.logoWhite)
+        .animate()
+        .slideY(
+          end: 1.2,
+        )
+        .scale();
   }
 }
