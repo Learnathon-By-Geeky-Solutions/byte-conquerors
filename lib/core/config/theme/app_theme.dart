0bc4efa75cedda853d5cc06bb3c3d432b4232d82
Md Alhaz Mondal Hredhay <amhredhay102@gmail.com>
2025-01-22 20:06:49 +0600

    Input decoration theme has been added on theme manager

--- lib/core/config/theme/app_theme.dart
+++ lib/core/config/theme/app_theme.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:soul_space/core/config/theme/app_colors.dart';
 
-class AppTheme {
+class ThemeManager {
+  ThemeManager._();
+
   static final lightTheme = ThemeData(
     primaryColor: AppColors.primaryPurple,
-    scaffoldBackgroundColor: AppColors.lightBackGround,
+    scaffoldBackgroundColor: Colors.white,
     brightness: Brightness.light,
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
@@ -19,7 +22,70 @@ class AppTheme {
         ),
       ),
     ),
+    appBarTheme: AppBarTheme(
+      titleTextStyle: const TextStyle(
+        color: Colors.white,
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+      ),
+      iconTheme: const IconThemeData(color: Colors.white),
+      titleSpacing: -5,
+      backgroundColor: Color(0xFF00B795),
+      elevation: 0,
+      surfaceTintColor: Colors.transparent,
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.light,
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent,
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.symmetric(
+        horizontal: 16.0,
+        vertical: 13.0,
+      ),
+      floatingLabelBehavior: FloatingLabelBehavior.auto,
+      labelStyle: TextStyle(
+        color: Color(0xFF00B795),
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+      ),
+      hintStyle: TextStyle(
+        color: Color(0xFF00B795),
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4.0),
+        borderSide: BorderSide(
+          color: Color(0xFF00B795),
+          width: 1.0,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4.0),
+        borderSide: BorderSide(
+          color: Color(0xFF00B795),
+          width: 1.0,
+        ),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4.0),
+        borderSide: BorderSide(
+          color: Colors.red,
+          width: 1.0,
+        ),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4.0),
+        borderSide: BorderSide(
+          color: Colors.red.shade400,
+          width: 1.0,
+        ),
+      ),
+    ),
   );
+
   static final darkTheme = ThemeData(
     primaryColor: AppColors.primaryPurple,
     scaffoldBackgroundColor: AppColors.darkBackGround,
@@ -35,6 +101,68 @@ class AppTheme {
         ),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(30.0),
+        ),
+      ),
+    ),
+    appBarTheme: AppBarTheme(
+      titleTextStyle: const TextStyle(
+        color: Colors.white,
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+      ),
+      iconTheme: const IconThemeData(color: Colors.white),
+      titleSpacing: -5,
+      backgroundColor: Color(0xFF00B795),
+      elevation: 0,
+      surfaceTintColor: Colors.transparent,
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.light,
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent,
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.symmetric(
+        horizontal: 16.0,
+        vertical: 13.0,
+      ),
+      floatingLabelBehavior: FloatingLabelBehavior.auto,
+      labelStyle: TextStyle(
+        color: Color(0xFF00B795),
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+      ),
+      hintStyle: TextStyle(
+        color: Color(0xFF00B795),
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4.0),
+        borderSide: BorderSide(
+          color: Color(0xFF00B795),
+          width: 1.0,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4.0),
+        borderSide: BorderSide(
+          color: Color(0xFF00B795),
+          width: 1.0,
+        ),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4.0),
+        borderSide: BorderSide(
+          color: Colors.red,
+          width: 1.0,
+        ),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4.0),
+        borderSide: BorderSide(
+          color: Colors.red.shade400,
+          width: 1.0,
         ),
       ),
     ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:soul_space/core/config/theme/app_theme.dart';
 import 'package:soul_space/features/splash_screen/presentation/pages/splash_page.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.light(),
+      theme: ThemeManager.lightTheme,
       home: const SplashPage(),
     );
   }
