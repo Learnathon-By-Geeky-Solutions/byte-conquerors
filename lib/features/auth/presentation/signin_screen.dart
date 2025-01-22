44f2ff4fa12d63f3cc2f13ebb5ee6b9bb509df1b
Md Alhaz Mondal Hredhay <amhredhay102@gmail.com>
2025-01-22 19:56:16 +0600

    ReusableTextFormField widget design has been done

--- lib/features/auth/presentation/signin_screen.dart
+++ lib/features/auth/presentation/signin_screen.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:soul_space/components/reusable_text_from_field_widget.dart';
 
-class SignInScreen extends StatelessWidget {
+class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController _mailTEController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('LogIn Screen'),
-            
-          ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('LogIn Screen'),
+              ReusableTextFormField(
+                hintText: 'Enter your email',
+                controller: _mailTEController,
+                keyboardType: TextInputType.emailAddress,
+                showLabelText: true,
+                onChanged: (_) {},
+              ),
+            ],
+          ),
         ),
       ),
     );
