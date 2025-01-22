36d554a1cb4ac1692fb76efe4bff0fde05b9295c
Md Alhaz Mondal Hredhay <amhredhay102@gmail.com>
2025-01-22 19:51:00 +0600

    Signin screen design in progress

---
+++ lib/features/auth/presentation/signin_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('LogIn Screen'),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
