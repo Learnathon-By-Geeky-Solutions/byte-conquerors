29c194b5ee8e1b23af88f1e57db42cd9b585b079
Dev Ruman <sayfurrahman324@gmail.com>
2025-02-11 21:44:15 +0600

    additional information updated for profile creation

--- lib/features/auth/presentation/pages/sign_in_page.dart
+++ lib/features/auth/presentation/pages/sign_in_page.dart
@@ -7,6 +7,7 @@ import 'package:soul_space/core/config/theme/app_colors.dart';
 import 'package:soul_space/features/auth/domain/repositories/auth_repository.dart';
 import 'package:soul_space/features/auth/presentation/pages/sign_up_page.dart';
 import 'package:soul_space/features/feature_name/presentation/pages/feature_page.dart';
+import 'package:soul_space/features/profile/presentation/pages/additional_info_page.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
---
+++ lib/features/profile/presentation/pages/edit_profile_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EditProfilePage extends StatelessWidget {
+  const EditProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Profile'),
+      ),
+      body: Center(
+        child: Text('Edit Profile Page'),
+      ),
+    );
+  }
+}
