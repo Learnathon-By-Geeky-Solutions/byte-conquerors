de33c7afdd4008a7fcaacdcee9d8e2bab3cd7df2
Md Alhaz Mondal Hredhay <amhredhay102@gmail.com>
2025-02-13 18:30:55 +0600

    Firebase setup has been done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:soul_space/core/config/theme/app_theme.dart';
 import 'package:soul_space/features/splash_screen/presentation/pages/splash_page.dart';
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeManager.lightTheme,
       home: const SplashPage(),
