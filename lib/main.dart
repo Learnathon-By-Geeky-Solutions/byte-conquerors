8b25f3e1d4153a948f3f9e0dd5b62eb96e11a96d
Md Alhaz Mondal Hredhay <amhredhay102@gmail.com>
2025-02-13 18:49:13 +0600

    Firebase signup implementation has been done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:soul_space/core/config/theme/app_theme.dart';
 import 'package:soul_space/features/splash_screen/presentation/pages/splash_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
