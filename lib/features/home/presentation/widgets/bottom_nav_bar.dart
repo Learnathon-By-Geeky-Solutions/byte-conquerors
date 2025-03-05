803c34254795a54c5387ef62da48846e9d410470
Dev Ruman <sayfurrahman324@gmail.com>
2025-03-05 17:55:35 +0600

    homepage file structure updated

---
+++ lib/features/home/presentation/widgets/bottom_nav_bar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.dashboard),
+          label: 'Dashboard',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: 'Settings',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.notifications),
+          label: 'Notifications',
+        ),
+      ],
+    );
+  }
+}
