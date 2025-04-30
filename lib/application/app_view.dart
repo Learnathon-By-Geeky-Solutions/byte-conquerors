eaf6198c0062620e3f5f537a3b78841651697540
- Ruman <sayfurrahman324@gmail.com>
2025-04-30 19:50:40 +0600

    feature updated

--- lib/application/app_view.dart
+++ lib/application/app_view.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:soul_space/application/app_start/bloc/app_start_bloc.dart';
 import 'package:soul_space/application/navigation/presentation/main_screen.dart';
 import 'package:soul_space/core/config/theme/app_size.dart';
 import 'package:soul_space/core/config/theme/app_theme.dart';
 import 'package:soul_space/features/auth/presentation/signin/pages/sign_in_page.dart';
+import 'package:soul_space/features/onboarding/presentation/pages/onboarding_page.dart';
+import 'package:soul_space/generated/l10n.dart';
 
 class SoulSpaceApp extends StatelessWidget {
   const SoulSpaceApp({super.key});
@@ -22,13 +25,14 @@ class SoulSpaceApp extends StatelessWidget {
         builder: (context, child) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
-            // localizationsDelegates: [
-            //   S.delegate,
-            //   GlobalMaterialLocalizations.delegate,
-            //   GlobalWidgetsLocalizations.delegate,
-            //   GlobalCupertinoLocalizations.delegate,
-            // ],
-            // supportedLocales: S.supportedLocales,
+            localizationsDelegates: [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            locale: S.supportedLocales.first,
+            supportedLocales: S.supportedLocales,
             themeMode: ThemeMode.system,
             theme: ThemeManager.lightTheme,
             darkTheme: ThemeManager.darkTheme,
@@ -38,8 +42,7 @@ class SoulSpaceApp extends StatelessWidget {
                   return const Scaffold(
                       body: Center(child: CircularProgressIndicator()));
                 } else if (state is AppStartShowOnboarding) {
-                  // return const OnboardingPage();
-                  return MainScreen();
+                  return const OnboardingPage();
                 } else if (state is AppStartShowLogin) {
                   return const SignInPage();
                 } else if (state is AppStartShowHome) {
